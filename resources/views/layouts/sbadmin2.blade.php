62fb450157ddfd890515fd160c0e250f9e22d02d
Agung <Agungislami27@gmail.com>
2023-12-26 11:03:54 +0700

    Dashboard

--- resources/views/layouts/sbadmin2.blade.php
+++ resources/views/layouts/sbadmin2.blade.php
@@ -43,7 +43,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{ route('home') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -65,8 +65,9 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
+                        <h6 class="collapse-header">Data Pendaftaran</h6>
+                        <a class="collapse-item" href="{{ route('pendaftaran.create') }}">Daftar</a>
+                        <a class="collapse-item" href="{{ route('pendaftaran.index') }}">Lihat</a> 
                         <a class="collapse-item" href="cards.html">Cards</a>
                     </div>
                 </div>
